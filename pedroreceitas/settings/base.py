950912cc2ff9562fbdf0a9ec8d793d88d8ae9015
Robson Leal <devleal@hotmail.com>
2022-01-27 22:05:07 -0300

    Adicionando imagens ao projeto

--- pedroreceitas/settings/base.py
+++ pedroreceitas/settings/base.py
@@ -125,6 +125,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
