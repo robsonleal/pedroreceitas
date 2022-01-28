42f4f99733a4e7fdabccba1e14de1bc49f7a654d
Robson Leal <devleal@hotmail.com>
2022-01-28 12:19:57 -0300

    Adicionando página de cadastro, login e dashboard

--- pedroreceitas/settings/base.py
+++ pedroreceitas/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'receitas',
     'pessoas',
+    'usuarios'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'pedroreceitas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ usuarios/views.py
@@ -0,0 +1,58 @@
+from pdb import post_mortem
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+# Create your views here.
+def cadastro(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password']
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco!')
+            return redirect('cadastro')
+        if not email.strip():
+            print('O campo email não pode ficar em branco!')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('Senhas estão diferentes!')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            print('Usuário já cadastrado!')
+            return redirect('cadastro')
+        
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        print('Usuário criado com sucesso!')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == '' or senha == '':
+            print('Os campos de login não podem ficar em branco!')
+            return redirect('login')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso')
+            return redirect('dashboard')
+
+    return render(request, 'usuarios/login.html')
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')
+    else:
+        return redirect('index')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
