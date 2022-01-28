62005aca54673295dcabef2f03a35d49e94acc69
Robson Leal <devleal@hotmail.com>
2022-01-27 21:36:41 -0300

    Melhorando funcionalidade do admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+class Listando_receitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'publicada')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_editable = ('publicada',)
+    list_per_page = 3
+
+admin.site.register(Receita, Listando_receitas)
--- receitas/models.py
+++ receitas/models.py
@@ -1,9 +1,11 @@
 from pyexpat import model
 from django.db import models
 from datetime import datetime
+from pessoas.models import Pessoa
 
 # Create your models here.
 class Receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
@@ -11,4 +13,5 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=50)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
+    publicada = models.BooleanField(default=False)
     
